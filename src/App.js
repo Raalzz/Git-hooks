6170fd070a789a506569ace92c6f841babcf7e29
Rahul Sawant <rahulsawanth06@gmail.com>
2021-05-20 17:44:34 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // below code is just to check if validate script is running fine or not with respect to eslint.
+  const showNames = () => {
+    const p = {
+      p1: "value1",
+      p2: "value2",
+      p3: "value3",
+    };
+
+    for (const key in p) {
+      if (p.hasOwnProperty(key)) {
+        console.log(`${key  } -> ${  p[key]}`);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dummy Project with Husky Setup</h1>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
 
 export default App;
+
+// Confused with below code. Should i replace below code with the code in .lintstagedrc file.?
+// {
+//   "*.{js,jsx}": ["npm run lint:fix --", "git add"],
+//   "*.{js,jsx,json}": ["prettier --write","git add"]
+// }
